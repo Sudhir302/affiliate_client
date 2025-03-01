7fe0e678ec52f3cca7e7af6d2f10430540e6b62b
Sudhir302 <cshakti302@gmail.com>
2025-03-01 14:57:18 +0530

    added css to footer

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import '../component/Products.css'
 import { useEffect, useState } from 'react';
-
+ 
 function Products(){
     let [products, setProducts] = useState([]);
 
